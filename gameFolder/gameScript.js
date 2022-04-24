a6e1c9cfeb568cdf980123e699867f8924ca3dd8
Eyen <eyentuyon@gmail.com>
2022-04-24 04:56:20 -0300

    Improve animation

--- gameFolder/gameScript.js
+++ gameFolder/gameScript.js
@@ -1,16 +1,21 @@
-
 const btn = document.querySelectorAll(".options");
 const points = document.querySelectorAll(".points");
 const announcer = document.querySelector("#announcer");
 const lines = document.querySelectorAll(".lines");
+const computerSelection = document.querySelector("#resultComputer");
 const rps = ["rock", "paper", "scissors"];
+const content = document.querySelector("#computer");
+const content2 = document.querySelector("#divisor");
+const content3 = document.querySelector("#human");
+const main = document.querySelector('#game')
+
+
 let computerPoints = 0;
 let humanPoints = 0;
 let result;
 let roundStage = 0;
 
 /*<------------------------------------------------------------------------------->*/
-
 function computerPlay() {
 
     function ranNumber() {
@@ -20,9 +25,7 @@ function computerPlay() {
 
     return rps[ranNumber()]
 }
-
 /*<------------------------------------------------------------------------------->*/
-
 function playRound(humanSelection, computerSelection) {
 
     if (humanSelection != "rock" && humanSelection != "paper" && humanSelection != "scissors") {
@@ -42,8 +45,38 @@ function playRound(humanSelection, computerSelection) {
         result = "You Lose";
     }
 }
-
 /*<------------------------------------------------------------------------------->*/
+function animation() {
+    const humanSelection = this.id;
+    const computerPlaySelec = computerPlay()
+
+    setTimeout(() => {
+        btn.forEach(button => button.classList.add("deleteAnimation"));
+        computerSelection.classList.add("deleteAnimation");
+    }, 500);
+    
+    setTimeout(() => {
+        btn[1].classList.remove('deleteAnimation');
+        btn[1].classList.add('optionSelection');
+        btn[1].innerHTML = `<img src="../Images/${humanSelection}.png" alt='${humanSelection}'></>`;
+        computerSelection.classList.remove('deleteAnimation');
+        computerSelection.classList.add('optionSelection');
+        computerSelection.style = 'rotate: 180deg;'
+        computerSelection.innerHTML = `<img src="../Images/${computerPlaySelec}.png" alt='${computerPlaySelec}'></>`;
+    }, 2000);
+  
+    setTimeout(() => game(humanSelection, computerPlaySelec), 3000)
+
+    setTimeout(() => {
+        btn.forEach(button => button.classList.remove("deleteAnimation"));
+        btn[1].classList.remove('optionSelection');
+        btn[1].innerHTML = `<img src="../Images/paper.png" alt='paper'></>`;
+        computerSelection.style = 'rotate: 0deg;'
+        computerSelection.classList.remove('optionSelection');
+        computerSelection.innerHTML = `（╬ಠ益ಠ）`;
+    }, 5500);
+
+}
 
 function announcerText() {
     announcer.textContent = result;
@@ -62,28 +95,9 @@ function announcerText() {
     }
 }
 
+function game(humanSelection, computerPlaySelec) {
 
-function removeClass(e) {
-    if (e.propertyName !== 'color' && e.propertyName !== "border-color") return;
-    this.classList.remove('announcerWin')
-    this.classList.remove('announcerLose')
-    this.classList.remove('announcerTie')
-
-    if (this.id == "announcer") {
-        announcer.textContent = "Select";
-    }
-
-}
-
-announcer.addEventListener("transitionend", removeClass)
-
-lines.forEach(line => line.addEventListener("transitionend", removeClass))
-
-function game() {
-    const humanSelection = this.id;
-    const computerSelection = computerPlay()
-
-    playRound(humanSelection, computerSelection)
+    playRound(humanSelection, computerPlaySelec)
 
     points.forEach(point => {
         if (point.id == "humanPoints") {
@@ -95,8 +109,29 @@ function game() {
 
 
     if (humanPoints == 5) {
+        setTimeout(() => [content, content2, content3].forEach(content => content.classList.add('deleteAnimation')), 2100)
+        setTimeout(() => [content, content2, content3].forEach(content => content.style = 'display: none ;'), 2900)
+        setTimeout(() => {
+            const retryMenu = document.createElement('div');
+            retryMenu.style = "display:flex; align-items:center; justify-content:center; flex-direction: column;"
+            retryMenu.innerHTML = `<h1 class="announcerWin" style = "font-size: 63px;">YOU WON!</h1> <button onClick="window.location.reload()" style = "cursor:pointer; border:none; border-radius: 17px; background-color:#0C1713; color:#65B891; font-size: 34px; padding: 20px;">retry?</button>`;
+            main.style = "display:flex; justify-content: center; flex-direction: none;"
+            main.appendChild(retryMenu);
+            
+        },3000)
         result = "YOU WON!";
+
     } else if (computerPoints == 5) {
+        setTimeout(() => [content, content2, content3].forEach(content => content.classList.add('deleteAnimation')), 2100)
+        setTimeout(() => [content, content2, content3].forEach(content => content.style = 'display: none ;'), 2900)
+        setTimeout(() => {
+            const retryMenu = document.createElement('div');
+            retryMenu.style = "display:flex; align-items:center; justify-content:center; flex-direction: column;"
+            retryMenu.innerHTML = `<h1 class="announcerLose" style = "font-size: 63px; transform: translateX(15px);">YOU LOST!</h1> <button onClick="window.location.reload()" style = "cursor:pointer; border:none; border-radius: 17px; background-color:#0C1713; color:#CC2936; font-size: 34px; padding: 20px;">retry?</button>`;
+            main.style = "display:flex; justify-content: center; flex-direction: none;"
+            main.appendChild(retryMenu);
+            
+        }, 3000)
         result = "YOU LOST!";
     }
 
@@ -104,9 +139,26 @@ function game() {
     
 }
 
-btn.forEach(button => button.addEventListener('click', game))
+btn.forEach(button => button.addEventListener('click', animation))
+
+btn.forEach(button => button.addEventListener('click', () => {
+    button.style = "pointer-events: none;"
+    setTimeout(() => button.style = "pointer-events: auto;", 5500);
+}))
 
 
+function removeClass(e) {
+    if (e.propertyName !== 'color' && e.propertyName !== "border-color") return;
+
+    this.classList.remove('announcerWin', 'announcerTie', 'announcerLose');
+
+    if (this.id == "announcer") {
+        announcer.textContent = "Select";
+    }
+
+}
+announcer.addEventListener("transitionend", removeClass)
+lines.forEach(line => line.addEventListener("transitionend", removeClass))
 
 
 
